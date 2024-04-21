bf5447d313f5a3d90404ddaed7dbdea34d68a80a
K-S-Abhihkeshav <97213738+K-S-Abhihkeshav@users.noreply.github.com>
2024-04-22 01:54:01 +0530

    Update and rename middle.sh to authentication.sh

--- Codes/cgi-bin/authentication.sh
+++ Codes/cgi-bin/authentication.sh
@@ -1,29 +1,62 @@
 #!/bin/bash
 
+
+
 # Print HTTP headers
+
 echo "Content-type: text/plain"
+
 echo ""
 
+
+
 # Extract filename from the curl command response
+
 cat > cred.txt
+
 echo "hi" >> log.txt
 
+
+
 username=$(grep -oP 'username=\K[^&]*' cred.txt)
-password=$(grep -oP 'password=\K.*' cred.txt)
-echo "$username $password" >> log.txt
+
+password=$(grep -oP 'password=\K[^&]*' cred.txt)
+
+FA=$(grep -oP 'FA=\K.*' cred.txt) #here
+
+echo "$username $password $FA" >> log.txt #here
+
 # Run Python script with the received username and password
+
 # Run Python script with input and capture output
 
-output=$(python ./checker.py "$username" "$password")
+
+
+output=$(python ./checker.py "$username" "$password" "$FA") #here
+
+
 
 # Print the output
+
 echo "$output" >> log.txt
+
+
 
 if [ "$output" != "good" ]; then
 
+
+
     # If output is not "good", return a 403 Forbidden status code
+
     echo '{"success": false}'
+
     echo "Error: Authentication failed" >> log.txt
+
 else
+
     echo '{"success": true}'
+
 fi
+
+
+
