46324915503dbb7aa806e0d2732486ca7c860ec4
K-S-Abhihkeshav <97213738+K-S-Abhihkeshav@users.noreply.github.com>
2024-04-22 01:53:19 +0530

    Update checker.py

--- Codes/cgi-bin/checker.py
+++ Codes/cgi-bin/checker.py
@@ -2,6 +2,42 @@
 
 import sys
 import json
+import hmac
+import time
+import base64
+import struct
+import hashlib
+
+
+def get_totp(secret_base64):
+    t = int(time.time()) // 30
+
+    #print("t=", t)
+
+    # Decode the secret from base64
+    secret_bytes = base64.b64decode(secret_base64)
+
+    # Calculate the required padding length
+    padding_length = (8 - len(secret_bytes) % 8) % 8
+
+    # Add padding to the secret
+    secret_padded = secret_bytes + b'\x00' * padding_length
+
+    # Convert the timestamp to bytes and pack it
+    counter = struct.pack('>Q', t)
+
+    # Calculate the HMAC-SHA1 hash
+    hash_result = hmac.new(secret_padded, counter, hashlib.sha1).digest()
+
+    # Extract the 4-byte dynamic truncation value
+    offset = hash_result[19] & 0xF
+    part = hash_result[offset:offset + 4]
+
+    # Unpack the 4-byte value and apply a mask
+    totp = (struct.unpack(">I", part)[0] & 0x7FFFFFFF) % 1000000
+
+    return str(totp).zfill(6)
+
 
 def main():
 
@@ -13,15 +49,16 @@ def main():
     # Retrieve username and password from command-line arguments
     username = sys.argv[1]
     hashed_password = sys.argv[2]
-   
+    FA = sys.argv[3]
+    
 
-    with open('users.json') as f:
+    with open('/home/kali/metadata/users.json') as f:
         users = json.load(f)
 
 
     # Check if username and hashed password match any user in the JSON data
     for user in users:
-        if user['username'] == username and user['password'] == hashed_password:
+        if user['username'] == username and user['password'] == hashed_password and FA==str(get_totp(user['FA'])):
             #print('Content-Type: application/json')  # Set the content type to JSON
             #print()  # Print a blank line to indicate the start of the response body
             #print(json.dumps({'success': True}))  # Send JSON response indicating success
